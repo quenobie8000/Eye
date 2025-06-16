e43db7cdd792f1d262a751eff0c65e8a7c403a96
quenobie8000 <136032697+quenobie8000@users.noreply.github.com>
2025-06-16 19:32:13 -0400

    tweaks

--- script.js
+++ script.js
@@ -7,21 +7,6 @@
 		let X;
 		let Y;
 
-		event = event || window.event;
-		if (event.pageX == null && event.clientX != null) {
-			eventDoc = (event.target && event.target.ownerDocument) || document;
-			doc = eventDoc.documentElement;
-			body = eventDoc.body;
-
-			event.pageX =
-				event.clientX +
-				((doc && doc.scrollLeft) || (body && body.scrollLeft) || 0) -
-				((doc && doc.clientLeft) || (body && body.clientLeft) || 0);
-			event.pageY =
-				event.clientY +
-				((doc && doc.scrollTop) || (body && body.scrollTop) || 0) -
-				((doc && doc.clientTop) || (body && body.clientTop) || 0);
-		}
 
 		var box = document.getElementById("box");
 		var limit = document.getElementById("limit");
